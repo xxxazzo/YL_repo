9a684ad0a0cd627a3e558f367401654bd402de73
Matthew <kjcxz9f937@yandex.ru>
2024-02-17 22:34:36 +0300

    Большая задача по Maps API. Часть №2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
 
 
 class YMAPS_WINDOW(QMainWindow):
@@ -14,30 +15,41 @@ class YMAPS_WINDOW(QMainWindow):
 
         self.pixmap = None
         self.map_file = None
+        self.scales_choices = []
         self.button.clicked.connect(self.getImage)
 
-    def getImage(self):
-        api_server = "http://static-maps.yandex.ru/1.x/"
-        map_params = {
-            "ll": self.coordinates.text().strip(),
-            "spn": self.scale.text().strip(),
-            "l": "map"
-        }
-        response = requests.get(api_server, params=map_params)
+    def getImage(self, **kwargs):
+        try:
+            k = kwargs.get('k', 0)
+            new_z = self.scale.value() + k
+            api_server = "http://static-maps.yandex.ru/1.x/"
+            map_params = {
+                "ll": self.coordinates.text().strip(),
+                "z": new_z if new_z in range(0, 22) else self.scale.value(),
+                "l": "map"
+            }
+            response = requests.get(api_server, params=map_params)
 
-        if not response:
-            self.statusBar().showMessage(f"Http статус:{response.status_code}({response.reason})")
-            print(f"Http статус:{response.status_code}({response.reason})")
-            print(response.url)
-            sys.exit(1)
-        else:
-            self.statusBar().showMessage("OK")
+            if not response:
+                self.statusBar().showMessage(f"Http статус: {response.status_code} ({response.reason})")
+            else:
+                self.statusBar().showMessage("OK")
 
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
-        self.pixmap = QPixmap(self.map_file)
-        self.map_image_label.setPixmap(self.pixmap)
+                self.map_file = "map.png"
+                with open(self.map_file, "wb") as file:
+                    file.write(response.content)
+                self.pixmap = QPixmap(self.map_file)
+                self.map_image_label.setPixmap(self.pixmap)
+                if new_z in range(0, 22):
+                    self.scale.setValue(map_params['z'])
+        except Exception as ex:
+            self.statusBar().showMessage(ex)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown and self.map_file is not None:
+            self.getImage(k=1)
+        elif event.key() == Qt.Key_PageUp and self.map_file is not None:
+            self.getImage(k=-1)
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
